d1ece014fac275345dd3d72929ed572cf8691c97
Cristiano Pires Martins <cpmart@gmail.com>
2023-03-14 16:01:17 -0300

    usando constante

--- ex08-vetor.c
+++ ex08-vetor.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#define N 5
+#define N 2000
 int main(){
-    int vetorIdade[5], i;
-    for(int i=0; i<5; i++){
+    int vetorIdade[N], i;
+    for(int i=0; i<N; i++){
         printf("Digite a idade (%d): ",i+1);
         scanf("%d",&vetorIdade[i]);
     }
-    for(int i=0; i<5; i++){
+    for(int i=0; i<N; i++){
         printf("Idade do %dº: %d\n",i+1,vetorIdade[i]);
     }
 }
