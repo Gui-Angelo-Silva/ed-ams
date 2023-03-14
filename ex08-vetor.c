24cb4598f537e256369730e50f5c470cf002f40d
Cristiano Pires Martins <cpmart@gmail.com>
2023-03-14 16:00:46 -0300

    exercício base

--- ex08-vetor.c
+++ ex08-vetor.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+#define N 5
 int main(){
-    
+    int vetorIdade[5], i;
+    for(int i=0; i<5; i++){
+        printf("Digite a idade (%d): ",i+1);
+        scanf("%d",&vetorIdade[i]);
+    }
+    for(int i=0; i<5; i++){
+        printf("Idade do %dº: %d\n",i+1,vetorIdade[i]);
+    }
 }
