7f5d197d0e5ff0581731cda7b3c5c042e8a4b1cf
Cristiano Pires Martins <cpmart@gmail.com>
2023-03-28 15:35:29 -0300

    invertido v3 uhuulll

--- ex14-vetor-invertido.c
+++ ex14-vetor-invertido.c
@@ -7,9 +7,7 @@ int main(){
     int vetA[N],vetB[N];
     srand(time(NULL));
     for(int i=0; i<N; i++){
-        vetA[i]=rand()%21;
-    }
-    for(int i=0; i<N; i++){
+        vetA[N-1-i]=rand()%21;
         vetB[i]= vetA[(N-1)-i];
         printf("vetA[%d] =%3d - vetB[%d] =%3d\n",N-1-i,vetA[N-1-i],i,vetB[i]);
     }
